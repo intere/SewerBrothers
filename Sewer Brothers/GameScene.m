be3a86c00182eae750cc22e54004394e3d2c9289
Eric Internicola <einternicola@silverchalice.com>
2015-04-25 16:49:11 -0600

    Sprite now walks left and right.  Refactoring effort is next.

--- Sewer Brothers/GameScene.m
+++ Sewer Brothers/GameScene.m
@@ -22,30 +22,42 @@
     for(UITouch *touch in touches) {
         CGPoint location = [touch locationInNode:self];
         
-        // 4 animation frames stored as textures
-        SKTexture *f1 = [SKTexture textureWithImageNamed:@"Player_Right1.png"];
-        SKTexture *f2 = [SKTexture textureWithImageNamed:@"Player_Right2.png"];
-        SKTexture *f3 = [SKTexture textureWithImageNamed:@"Player_Right3.png"];
-        SKTexture *f4 = [SKTexture textureWithImageNamed:@"Player_Right4.png"];
-        
-        // an arrray of these textures
-        NSArray *textureArray = @[f1, f2, f3, f4];
-        
-        // our player character sprite & starting position in the scene
-        _playerSprite = [SKSpriteNode spriteNodeWithTexture:f1];
-        _playerSprite.position = location;
-        
-        // An Action using our array of textures iwth each frame lasting 0.1 seconds
-        SKAction *runRightAction = [SKAction animateWithTextures:textureArray timePerFrame:0.1];
-        
-        // don't run just once, but loop indefinitely
-        SKAction *runForever = [SKAction repeatActionForever:runRightAction];
-        
-        // attach the completed action to our sprite
-        [_playerSprite runAction:runForever];
-        
-        // add the sprite to the scene
-        [self addChild:_playerSprite];
+        if(!_playerSprite) {
+            // 4 animation frames stored as textures
+            SKTexture *f1 = [SKTexture textureWithImageNamed:@"Player_Right1.png"];
+            SKTexture *f2 = [SKTexture textureWithImageNamed:@"Player_Right2.png"];
+            SKTexture *f3 = [SKTexture textureWithImageNamed:@"Player_Right3.png"];
+            SKTexture *f4 = [SKTexture textureWithImageNamed:@"Player_Right4.png"];
+            
+            // an arrray of these textures
+            NSArray *textureArray = @[f1, f2, f3, f4];
+            
+            // our player character sprite & starting position in the scene
+            _playerSprite = [SKSpriteNode spriteNodeWithTexture:f1];
+            _playerSprite.position = location;
+            
+            // An Action using our array of textures iwth each frame lasting 0.1 seconds
+            SKAction *runRightAction = [SKAction animateWithTextures:textureArray timePerFrame:0.1];
+            
+            // don't run just once, but loop indefinitely
+            SKAction *runForever = [SKAction repeatActionForever:runRightAction];
+            
+            // attach the completed action to our sprite
+            [_playerSprite runAction:runForever];
+            
+            // add the sprite to the scene
+            [self addChild:_playerSprite];
+        } else if(location.x <= (self.frame.size.width / 2)) {
+            NSLog(@"Left Tap");
+            SKAction *moveLeft = [SKAction moveByX:-100 y:0 duration:1];
+            SKAction *moveForever = [SKAction repeatActionForever:moveLeft];
+            [_playerSprite runAction:moveForever];
+        } else {
+            NSLog(@"Right Tap");
+            SKAction *moveRight = [SKAction moveByX:100 y:0 duration:1];
+            SKAction *moveForever = [SKAction repeatActionForever:moveRight];
+            [_playerSprite runAction:moveForever];
+        }
     }
 }
 
